efb107483222e75896f311a3fd84e71343c0b17e
Nicolas Dasriaux <nicolas.dasriaux@carbon-it.com>
2019-08-26 15:16:37 +0200

    Embedded instructions for Reading JSON

--- src/test/scala/ReadingJsonSpec.scala
+++ src/test/scala/ReadingJsonSpec.scala
@@ -2,6 +2,17 @@ import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
+/**
+ * Reading JSON
+ *
+ * (1) Run the test class.
+ *     Eventually it will block in [[ReadingJsonSpec.afterAll]] on [[SparkPerf.keepSparkUIAlive()]] keeping Spark UI alive.
+ *
+ * (2) Open Spark UI in browser [[http://localhost:4040]]
+ *
+ * (3) Follow instructions for each of the test cases
+ */
+
 class ReadingJsonSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   val sparkSession: SparkSession = SparkSession.builder()
     .appName("Reading JSON")
@@ -59,6 +70,26 @@ class ReadingJsonSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   }
 
   "Reading a JSON Lines file" should "be parallelizable" in {
+    /**
+     * (4) Read about JSON Lines format [[http://jsonlines.org/]]
+     *
+     * (5) Take a look at `hdfs/customers.jsonl` directory
+     *     - Notice single part
+     *     - Notice presence of a JSON object per line without LF in the middle of line
+     *
+     * (6) Map **Queries** to code lines of this test using line number (and map queries to jobs)
+     *     - `collect` method call line
+     *
+     * (7) Map **Jobs** to code lines of this test using line number
+     *     - `collect` method call line
+     *
+     * (8) Map **Stages** to code lines of this test using line number
+     *     - `collect` method call line
+     *
+     * (9) Identify how many tasks are used to:
+     *     - Read JSON Lines file
+     */
+
     implicit val spark: SparkSession = sparkSession
 
     val customerSchema = StructType(
@@ -76,6 +107,24 @@ class ReadingJsonSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   }
 
   "Reading a JSON file" should "not be parallelizable" in {
+    /**
+     * (10) Take a look at `hdfs/customers.json` directory
+     *      - Notice single part
+     *      - Notice content consists of a JSON array containing JSON objects (with LFs sprinkled out here and there)
+     *
+     * (11) Map **Queries** to code lines of this test using line number (and to which Jobs they map to)
+     *      - `collect` method call line
+     *
+     * (12) Map **Jobs** to code lines of this test using line number
+     *      - `collect` method call line
+     *
+     * (13) Map **Stages** to code lines of this test using line number
+     *      - `collect` method call line
+     *
+     * (14) Identify how many tasks are used to:
+     *      - Read JSON file
+     */
+
     implicit val spark: SparkSession = sparkSession
 
     val customerSchema = StructType(
@@ -94,6 +143,23 @@ class ReadingJsonSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   }
 
   "Reading a JSON Lines file with schema inference" should "add an additional full scan before reading" in {
+    /**
+     * (15) Map **Queries** to code lines of this test using line number (and to which Jobs they map to)
+     *      - `collect` method call line
+     *
+     * (16) Map **Jobs** to code lines of this test using line number
+     *      - `json` method call line
+     *      - `collect` method call line
+     *
+     * (17) Map **Stages** to code lines of this test using line number
+     *      - `json` method call line
+     *      - `collect` method call line
+     *
+     * (18) Identify how many tasks are used and how many records are read to:
+     *      - Infer schema
+     *      - Read JSON Lines file
+     */
+
     implicit val spark: SparkSession = sparkSession
 
     val customersDF = spark.read
